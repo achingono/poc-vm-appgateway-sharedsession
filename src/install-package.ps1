02db05f90e964f92eef3b9d0030e116a45659112
Alfero Chingono <2288092+achingono@users.noreply.github.com>
2024-02-12 18:53:37 -0500

    Add Redis client installation and registration

--- src/install-package.ps1
+++ src/install-package.ps1
@@ -10,3 +10,10 @@ nuget install Microsoft.Web.RedisSessionStateProvider -OutputDirectory ./package
 Get-ChildItem -Path ./packages -Recurse | Where-Object { $_.FullName -match "\\lib\\net4.*\\.*\.dll"} | ForEach-Object {
     Copy-Item -Path $_.FullName -Destination ./bin
 }
+# download redis-com-client-master.zip
+Invoke-WebRequest https://gitlab.com/erik4/redis-com-client/-/archive/master/redis-com-client-master.zip?path=Precompiled%20DLLs -OutFile redis-com-client-master.zip
+# unzip redis-com-client-master.zip
+Expand-Archive -Path redis-com-client-master.zip -DestinationPath ./bin
+# register redis-com-client.dll
+# https://gitlab.com/erik4/redis-com-client/-/tree/master?ref_type=heads#installation
+& c:\Windows\Microsoft.NET\Framework64\v4.0.30319\regasm .\bin\redis-com-client.dll /tlb:redis-com-client.tlb /codebase 
