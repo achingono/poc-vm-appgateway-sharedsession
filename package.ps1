5c76810ba60c1deeaddd676f7563cd0c1ab7f539
Alfero Chingono <2288092+achingono@users.noreply.github.com>
2024-02-17 18:18:50 -0800

    Refactor package.ps1 script to improve readability and maintainability

--- package.ps1
+++ package.ps1
@@ -1,47 +1,79 @@
 
-$siteCodeFolder = $PSScriptRoot
+$siteCodeFolder = "$PSScriptRoot\src";
 $sitePackageFolder = "$((Get-Item $siteCodeFolder).Parent.FullName)\pkg";
+$nugetPath = "$((Get-Item $siteCodeFolder).Parent.FullName)\.nuget";
 $sourceNugetExe = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe";
 $targetNugetExe = "$nugetPath\nuget.exe";
+$nugetPackagesPath = "$sitePackageFolder\nuget";
+$binPath = "$siteCodeFolder\bin";
+$sourceConfig = "$siteCodeFolder\web.config";
+$transformConfig = "$siteCodeFolder\web.release.config";
+$targetConfig = "$siteCodeFolder\web.config";
 
-# download the nuget binary
-mkdir -Path $nugetPath
-Invoke-WebRequest $sourceNugetExe -OutFile $targetNugetExe
-Set-Alias nuget $targetNugetExe -Scope Global -Verbose
-# install nuget package
-nuget install Microsoft.Web.RedisSessionStateProvider -OutputDirectory $nugetPackagesPath -Framework net46
-# copy binaries to bin folder
-Get-ChildItem -Path $nugetPackagesPath -Recurse | Where-Object { $_.FullName -match "\\lib\\net4.*\\.*\.dll"} | ForEach-Object {
-    Copy-Item -Path $_.FullName -Destination $binPath
+# create the .nuget folder if it doesn't exist
+if (!(Test-Path $nugetPath)) {
+    mkdir -Path $nugetPath;
+}
+
+# download the nuget binary if it doesn't exist
+if (!(Test-Path $targetNugetExe)) {
+    Invoke-WebRequest $sourceNugetExe -OutFile $targetNugetExe;
 }
 
 # install nuget packages
-nuget install Microsoft.Web.Xdt -OutputDirectory $nugetPackagesPath
-nuget install SlowCheetah.Xdt -OutputDirectory $nugetPackagesPath
+& $targetNugetExe install -SolutionDirectory $siteCodeFolder -OutputDirectory $nugetPackagesPath;
 
-# locate the SlowCheeter executable
-$transformerPath = (Get-ChildItem -Path $nugetPackagesPath -Include 'SlowCheetah.Xdt.exe' -Recurse) | Select-Object -First 1
-
-# locate the Xdt dll
-$xdtPath = (Join-Path $transformerPath.Directory.FullName 'Microsoft.Web.XmlTransform.dll')
-if(!(Test-Path $xdtPath)){
-    # copy the xdt.dll next to the slowcheetah .exe
-    $xdtDll = (Get-ChildItem -Path $nugetPackagesPath -Include 'Microsoft.Web.XmlTransform.dll' -Recurse) | Select-Object -First 1
-
-    if(!$xdtDll){ throw 'Microsoft.Web.XmlTransform.dll not found' }
-
-    Copy-Item -Path $xdtDll.Fullname -Destination $xdtPath | Out-Null
+# copy binaries to bin folder
+Get-ChildItem -Path $nugetPackagesPath -Recurse | Where-Object { $_.FullName -match "\\lib\\net4.*\\.*\.dll" } | ForEach-Object {
+    Copy-Item -Path $_.FullName -Destination $binPath;
 }
 
-# Transform web.config
+# locate the Xdt dll
+$xdtPath = (Get-ChildItem -Path $nugetPackagesPath -Include 'Microsoft.Web.XmlTransform.dll' -Recurse) | Select-Object -Last 1;
+if (!$xdtPath) { throw 'Microsoft.Web.XmlTransform.dll not found' };
+
+# load the Xdt dll
+Add-Type -Path $xdtPath.FullName;
+
+# Transform config file
 # https://gist.github.com/sayedihashimi/f1fdc4bfba74d398ec5b
-# slowcheetah.xdt.exe <source file> <transform> <dest file>
-& $transformerPath "$siteCodeFolder\web.config" "$siteCodeFolder\web.release.config" "$siteCodeFolder\web.config";
+Write-Host "Loading $sourceConfig";
+$document = New-Object Microsoft.Web.XmlTransform.XmlTransformableDocument;
+$document.PreserveWhitespace = $true;
+$document.Load($sourceConfig);
+
+Write-Host "Applying $transformConfig"
+$transform = New-Object Microsoft.Web.XmlTransform.XmlTransformation($transformConfig);
+
+$succeed = $transform.Apply($document);
+
+if (-not $succeed) {
+    throw ("Failed to apply transformation");
+}
+
+# if the target config exists, copy it to a backup
+if (Test-Path $targetConfig) {
+    Copy-Item -Path $targetConfig -Destination "$targetConfig.bak" -Force;
+}
+
+Write-Host "Saving $targetConfig"
+$document.Save($targetConfig);
 
 # create deployment package 
 # https://learn.microsoft.com/en-us/previous-versions/windows/it-pro/windows-server-2008-r2-and-2008/dd569054(v=ws.10)
 # https://learn.microsoft.com/en-us/previous-versions/windows/it-pro/windows-server-2008-r2-and-2008/dd569019(v=ws.10)
 & 'C:\Program Files (x86)\IIS\Microsoft Web Deploy V3\msdeploy.exe' -verb:sync `
-        -source:iisApp="$siteCodeFolder",includeAcls=false,enable32BitAppOnWin64=false,managedPipelineMode=Integrated,managedRuntimeVersion='v4.0' `
-        -dest:package="$sitePackageFolder\package.zip" `
-        -declareParamFile="$siteCodeFolder\parameters.xml"
+    -source:iisApp="$siteCodeFolder",includeAcls=false,enable32BitAppOnWin64=false,managedPipelineMode=Integrated,managedRuntimeVersion='v4.0' `
+    -dest:package="$sitePackageFolder\source.zip" `
+    -declareParamFile="$siteCodeFolder\parameters.xml" `
+    -skip:objectName=filePath,absolutePath=".*\\web.config.bak$" `
+    -skip:objectName=filePath,absolutePath=".*\\web.release.config$" `
+    -skip:objectName=filePath,absolutePath=".*\\nuget.config$" `
+    -skip:objectName=filePath,absolutePath=".*\\packages.config$" `
+    -skip:objectName=filePath,absolutePath=".*\\parameters.xml$";
+
+# if backup file exists, restore the original config from backup
+if (Test-Path "$targetConfig.bak") {
+    Copy-Item -Path "$targetConfig.bak" -Destination $targetConfig -Force;
+    Remove-Item -Path "$targetConfig.bak" -Force;
+}
